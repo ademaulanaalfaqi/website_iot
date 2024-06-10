7593c5e4fef504995f36575e2d03ff0f7a88c8b6
Ade Maulana Alfaqi <107929610+ademaulanaalfaqi@users.noreply.github.com>
2024-06-10 15:02:24 +0700

    radial chart

--- system/resources/views/admin/ade/index.blade.php
+++ system/resources/views/admin/ade/index.blade.php
@@ -4,11 +4,11 @@
         <div class="col-md-5 mb-3">
             <div class="card mb-3">
                 <div class="card-body">
-                    <h5 class="card-title">Debit Air</h5>
+                    <h5 class="card-title">Statistik Debit Air</h5>
                     <div class="container text-center">
                         <div class="row align-items-center">
                             <div class="col border p-1">
-                                <p class="fs-1 m-0" id="apiText" style="color: #002ca3"></p>
+                                <p class="fs-1 m-0" id="apiText" style="color: #566a7f"></p>
                             </div>
                             <p class="m-0 mt-2" style="font-weight: bold">L/min</p>
                         </div>
@@ -24,13 +24,13 @@
         <div class="col-md-7 mb-3">
             <div class="card mb-3">
                 <div class="card-body">
-                    <h5 class="card-title">Tekanan Air</h5>
+                    <h5 class="card-title">Statistik Tekanan Air</h5>
                     <div class="container text-center">
                         <div class="row align-items-center">
                             <div class="col border p-1">
-                                <p class="fs-1 m-0" id="pressureText" style="color: #002ca3"></p>
+                                <p class="fs-1 m-0" id="pressureText" style="color: #566a7f"></p>
                             </div>
-                            <p class="m-0 mt-2" style="font-weight: bold">Pa</p>
+                            <p class="m-0 mt-2" style="font-weight: bold">psi</p>
                         </div>
                     </div>
                 </div>
@@ -39,7 +39,7 @@
                 <div class="col-md-4 mb-3">
                     <div class="card">
                         <div class="card-body">
-                            <div id="radialChart"></div>
+                            <div id="gaugeChart"></div>
                         </div>
                     </div>
                 </div>
@@ -158,98 +158,94 @@
             // Set interval untuk memperbarui data setiap beberapa detik
             setInterval(displayAPIText, 10000); // Ganti dengan interval yang sesuai
         });
-
-
     </script>
 
     {{-- script radial bar tekanan --}}
     <script>
-        // Warna dan konfigurasi chart
-        const chartColors = {
-            cardBackground: '#f5f5f5', // Ganti dengan warna latar belakang card
-            textColor: '#333', // Ganti dengan warna teks
-            primary: '#099486' // Ganti dengan warna utama
-        };
+        function hitungPersentase(value, max) {
+            return ((value/max)*100);
+        }
 
-        // Pengaturan dasar chart
-        const options = {
-            series: [70], // Data yang ditampilkan
-            chart: {
-                type: 'radialBar', // Tipe chart
-                height: 240 // Tinggi chart
-            },
-            labels: ['Tekanan'], // Label chart
-            plotOptions: {
-                radialBar: {
-                    startAngle: -130, // Sudut awal chart
-                    endAngle: 130, // Sudut akhir chart
-                    offsetY: 10, // Offset vertikal
-                    hollow: {
-                        size: '55%' // Ukuran hollow di tengah
-                    },
-                    track: {
-                        background: chartColors.cardBackground, // Warna latar lintasan
-                        strokeWidth: '100%' // Lebar garis lintasan
-                    },
-                    dataLabels: {
-                        name: {
-                            fontSize: '15px', // Ukuran font label nama
-                            fontWeight: 600, // Ketebalan font label nama
-                            offsetY: 15, // Offset vertikal label nama
-                            color: chartColors.textColor, // Warna label nama
-                            fontFamily: 'Public Sans' // Keluarga font
-                        },
-                        value: {
-                            fontSize: '20px', // Ukuran font label nilai
-                            fontWeight: 500, // Ketebalan font label nilai
-                            offsetY: -25, // Offset vertikal label nilai
-                            color: chartColors.textColor, // Warna label nilai
-                            fontFamily: 'Public Sans' // Keluarga font
-                        }
-                    }
-                }
-            },
-            fill: {
-                type: 'gradient',
-                gradient: {
-                    shade: 'dark',
-                    shadeIntensity: 0.5,
-                    gradientToColors: [chartColors.primary],
-                    inverseColors: true,
-                    opacityFrom: 1,
-                    opacityTo: 0.5,
-                    stops: [30, 70, 100]
-                }
-            },
-            stroke: {
-                dashArray: 5 // Pola garis putus-putus
-            },
-            grid: {
-                padding: {
-                    top: -35,
-                    bottom: -10
-                }
-            },
-            states: {
-                hover: {
-                    filter: {
-                        type: 'none'
+        async function fetchPressureData() {
+            try {
+                const response = await fetch('/api/pressure_text');
+                const data = await response.json();
+                return data;
+            } catch (error) {
+                console.error('Error fetching pressure data:', error);
+                return null;
+            }
+        }
+
+        let chart;
+
+        async function inisialisasiChart() {
+            const nilaiPressure = await fetchPressureData();
+            const maxPressure = 50;
+            var hasilPersentase = hitungPersentase(nilaiPressure, maxPressure);
+            // console.log(nilaiPressure);
+    
+            var options = {
+                series: [hasilPersentase],
+                chart: {
+                    type: 'radialBar',
+                    offsetY: -20,
+                    sparkline: {
+                        enabled: true
                     }
                 },
-                active: {
-                    filter: {
-                        type: 'none'
+                plotOptions: {
+                    radialBar: {
+                        startAngle: -90,
+                        endAngle: 90,
+                        track: {
+                            background: "#e7e7e7",
+                            strokeWidth: '97%',
+                            margin: 5,
+                            dropShadow: {
+                                enabled: true,
+                                top: 2,
+                                left: 0,
+                                color: '#999',
+                                opacity: 1,
+                                blur: 2
+                            }
+                        },
+                        dataLabels: {
+                            name: {
+                                show: false
+                            },
+                            value: {
+                                offsetY: -2,
+                                fontSize: '16px',
+                                formatter: function(val) {
+                                    return val + '%';
+                                }
+                            }
+                        }
                     }
-                }
+                },
+                grid: {
+                    padding: {
+                        top: -10
+                    }
+                },
+                fill: {
+                    colors: ['#099486']
+                },
+                labels: ['Average Results'],
+            };
+
+            if (!chart) {
+                chart = new ApexCharts(document.querySelector("#gaugeChart"), options);
+                chart.render();
+            } else {
+                // Update chart dengan data baru
+                chart.updateSeries([hasilPersentase]);
             }
-        };
-
-        // Inisialisasi chart
-        const chartElement = document.querySelector('#radialChart');
-        const radialChart = new ApexCharts(chartElement, options);
-
-        // Render chart
-        radialChart.render();
+        }
+        setInterval(inisialisasiChart, 10000);
+        inisialisasiChart();
     </script>
 
     {{-- script chart tekanan --}}
@@ -326,7 +322,7 @@
         updateChartPressure();
     </script>
 
-    {{-- script text flow --}}
+    {{-- script text tekanan --}}
     <script>
         $(document).ready(function() {
             // Fungsi untuk mengambil data dari API dan menampilkannya
