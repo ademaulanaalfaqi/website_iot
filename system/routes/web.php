d1ce5272a6406147e9d082af4193fb736c63a807
Ade Maulana Alfaqi <107929610+ademaulanaalfaqi@users.noreply.github.com>
2024-08-11 20:41:16 +0700

    Update web.php

--- system/routes/web.php
+++ system/routes/web.php
@@ -13,9 +13,6 @@ use App\Http\Controllers\Ade\TekananController;
 use App\Http\Controllers\Jupi\PelangganController;
 
 
-use App\Http\Controllers\AuthController;
-use App\Http\Controllers\Jupi\PelangganController;
-
 
 /*
 |--------------------------------------------------------------------------
@@ -86,16 +83,15 @@ Route::middleware(['auth'])->group(function () {
     Route::post('/edit-pelanggan/{id}', [PelangganController::class, 'update']);
     Route::get('/hapus-data-pelanggan/{id}', [PelangganController::class, 'destroy']);
     Route::post('export-data-sensor-pelanggan/{id}', [PelangganController::class, 'exportDataPelanggan']);
-});
 
 
-Route::get('/ph', [PhController::class, 'ph']);
-Route::post('/tambah-sensor-ph', [PhController::class, 'menambahkanSensorPh']);
-Route::get('/detail-sensor-ph/{ph}', [PhController::class, 'detailSensorPh']);
-Route::put('/edit-sensor-ph/{ph}', [PhController::class, 'updateSensorPh']);
-Route::delete('/hapus-sensor-ph/{ph}', [PhController::class, 'hapusSensorPh']);
-Route::get('/download-today-report-ph/{ph}', [PhController::class, 'downloadTodayReportPh']);
-// Route::get('/download/reports/ph', [PhController::class, 'downloadReportsPh'])->name('download.reportsPh');
+    Route::get('/ph', [PhController::class, 'ph']);
+    Route::post('/tambah-sensor-ph', [PhController::class, 'menambahkanSensorPh']);
+    Route::get('/detail-sensor-ph/{ph}', [PhController::class, 'detailSensorPh']);
+    Route::put('/edit-sensor-ph/{ph}', [PhController::class, 'updateSensorPh']);
+    Route::delete('/hapus-sensor-ph/{ph}', [PhController::class, 'hapusSensorPh']);
+    Route::get('/download-today-report-ph/{ph}', [PhController::class, 'downloadTodayReportPh']);
+    // Route::get('/download/reports/ph', [PhController::class, 'downloadReportsPh'])->name('download.reportsPh');
 
     Route::post('download-today-report-turbi/{turbi}', [TurbiController::class, 'downloadTodayReportTurbi']);
     Route::post('download-reports-turbi/{turbi}', [TurbiController::class, 'downloadReportsTurbi']);
@@ -109,14 +105,4 @@ Route::get('/download-today-report-ph/{ph}', [PhController::class, 'downloadToda
     Route::post('download-reports-ph/{ph}', [PhController::class, 'downloadReportsPh']);
 });
 
-
-// route::get('/detail-sensor-meteran/{id}', [MeteranController::class, 'detailSensorMeteran']);
-Route::get('/meteran', [PelangganController::class, 'meteran']);
-Route::get('/meter-data/{id}', [PelangganController::class, 'chartMeter']);
-Route::get('/pelanggan', [PelangganController::class, 'index']);
-Route::get('/tambah-pelanggan', [PelangganController::class, 'create']);
-Route::post('/tambah-pelanggan', [PelangganController::class, 'store']);
-Route::get('/detail-pelanggan/{id}', [PelangganController::class, 'detail']);
-Route::get('/edit-pelanggan/{id}', [PelangganController::class, 'edit']);
-Route::post('/edit-pelanggan/{id}', [PelangganController::class, 'update']);
 
